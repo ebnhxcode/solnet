c7b359462a49214e9f038f008e44daa98e9cd687
vittoh <v.garrido.sepulveda@gmail.com>
2017-09-21 00:09:36 -0300

    Add files via upload

---
+++ src/pages/formulario/formulario.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicPage , NavController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@IonicPage()
+@Component({
+  selector: 'page-form',
+  templateUrl: 'my-form.html',
+})
+export class MyFormPage {
+
+  myForm: FormGroup;
+  
+  constructor(
+    public navCtrl: NavController,
+    public formBuilder: FormBuilder
+  ) {
+    this.myForm = this.createMyForm();
+  }
+  
+  saveData(){
+    console.log(this.myForm.value);
+  }
+  
+  private createMyForm(){
+    return this.formBuilder.group({
+      nombre: ['', Validators.required],
+      apellido_p: ['', Validators.required],
+      apellido_m: ['', Validators.required],
+      dateBirth: ['', Validators.required],
+      fono: ['', Validators.required],
+      email: ['', Validators.required],
+    });
+  }
+}
